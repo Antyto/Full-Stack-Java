4abffb460ee362bb2f3611656cb7beeec01830e8
Antyto <antonietanovoa91@gmail.com>
2021-06-10 13:17:18 -0400

    Proyectos

--- Java Fundamentals/Manipulacion/ManipulacionCadenas.java
+++ Java Fundamentals/Manipulacion/ManipulacionCadenas.java
@@ -22,4 +22,8 @@ public class ManipulacionCadenas {
             return null;
         }
     }
+    
+    public String concatSubstring(String str1, int a, int b, String str2){
+        return str1.substring(a, b).concat(str2);
+    }
 }
