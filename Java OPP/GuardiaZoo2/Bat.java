016e90e9a51f59eb114035e1847ac6ed18e29320
Antyto <antonietanovoa91@gmail.com>
2021-06-22 12:04:08 -0400

    Proyectos

--- Java OPP/GuardiaZoo2/Bat.java
+++ Java OPP/GuardiaZoo2/Bat.java
@@ -5,19 +5,16 @@ public class Bat extends Mammal {
 		this.setEnergia(300);
 	}
 	
-	//Print the sound a bat taking off and decrease its energy by 50.
 	public void fly() {
 		System.out.println("Alucard comienza a volar... El sonido producido por sus alas es ensordecedor");
 		this.setEnergia(this.getEnergia() - 50);
 	}
 	
-	//Print the so- well, never mind, just increase its energy by 25:
 	public void eatHumans() {
 		System.out.println("Alucard ha atacado un humano y se lo ha comido!!! ... Bueno.. no importa");
 		this.setEnergia(this.getEnergia() + 25);
 	}
 	
-	//Print the sound of a town on fire and decrease its energy by 100:
 	public void attackTown() {
 		System.out.println("Alucard canaliza su rabia destruyendo todo a su paso... Las explosiones se escuchan por todos lados ... la caida de la ciudad es inminente");
 		this.setEnergia(this.getEnergia() - 50);
