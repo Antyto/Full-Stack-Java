bb327057dabf03130dac0a3821967bc2529e0b60
Antyto <antonietanovoa91@gmail.com>
2021-07-21 10:29:18 -0400

    Proyectos

---
+++ Spring Data I/lookify/src/test/java/com/antyto/lookify/LookifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antyto.lookify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
